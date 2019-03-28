43a9c826a7ec641b1f9e8af46049b93eed8435ea
Rafael Luiz Casa <rafaelcasa.2007@yahoo.com.br>
2019-03-28 16:27:12 -0300

    Classe principal do sistema

---
+++ src/main/java/br/edu/ifrs/farroupilha/sigprod2/backend/Main.java
@@ -0,0 +1,18 @@
+package br.edu.ifrs.farroupilha.sigprod2.backend;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+/**
+ * Classe Principal do Sistema
+ *
+ * @author Rafael Luiz Casa
+ */
+public class Main {
+
+    private static final Logger LOGGER = LogManager.getLogger(Main.class.getName());
+
+    public static void main(String[] args) {
+        LOGGER.error("AINDA NAO DESENVOLVIDO");
+    }
+}
