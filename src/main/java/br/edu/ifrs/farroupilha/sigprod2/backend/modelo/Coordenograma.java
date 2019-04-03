261151e58924be4ed293b8ba771115c0a3f52c6d
Rafael Luiz Casa <rafaelcasa.2007@yahoo.com.br>
2019-04-03 14:05:46 -0300

    Mudar posição da legenda do coordenograma para canto superior esquerdo

--- src/main/java/br/edu/ifrs/farroupilha/sigprod2/backend/modelo/Coordenograma.java
+++ src/main/java/br/edu/ifrs/farroupilha/sigprod2/backend/modelo/Coordenograma.java
@@ -13,7 +13,6 @@ import org.knowm.xchart.XChartPanel;
 import org.knowm.xchart.XYChart;
 import org.knowm.xchart.XYChartBuilder;
 import org.knowm.xchart.XYSeries;
-import org.knowm.xchart.internal.chartpart.Chart;
 import org.knowm.xchart.style.Styler;
 import org.knowm.xchart.style.markers.SeriesMarkers;
 
@@ -37,7 +36,7 @@ public class Coordenograma {
         this.chart = new XYChartBuilder().title(this.titulo).xAxisTitle("Corrente").yAxisTitle("Tempo").build();
         this.chart.getStyler().setYAxisLogarithmic(true);
         this.chart.getStyler().setXAxisLogarithmic(true);
-        this.chart.getStyler().setLegendPosition(Styler.LegendPosition.OutsideE);
+        this.chart.getStyler().setLegendPosition(Styler.LegendPosition.InsideNE);
         this.chart.getStyler().setToolTipsEnabled(true);
         this.chart.getStyler().setToolTipsAlwaysVisible(true);
         this.chart.getStyler().setChartBackgroundColor(UIManager.getColor("Panel.background"));
